b5ff152884a17b3448be00034d33165f7ebd1287
wiem-hichri <wiemhichri794@gmail.com>
2025-01-13 14:54:11 +0100

    Initialisation du projet plateforme SMS

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API SMS Lumière');
+});
+
+app.listen(port, () => {
+    console.log(`Serveur Node.js démarré sur http://localhost:${port}`);
+});
