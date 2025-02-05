d8cc33878239a6fdca8716d04f2a1fff3c9cce9c
wiem-hichri <wiemhichri794@gmail.com>
2025-02-05 15:44:16 +0100

    last modificatin

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF', // Couleur bleue personnalisée
+      },
+    },
+  },
+  plugins: [],
+}
