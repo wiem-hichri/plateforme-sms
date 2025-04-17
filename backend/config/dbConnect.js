426c7ea6acce0b3c3145a8f22bf42b89d5fe19af
wiem-hichri <wiemhichri794@gmail.com>
2025-04-17 19:17:27 +0200

    simple update

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -6,8 +6,10 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    connectTimeout: 10000 // 10 secondes
 });
+
 
 connection.connect((err) => {
     if (err) {
