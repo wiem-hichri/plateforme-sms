36d97cdeb333b906100d08d5a941b9c39bd9e5d4
wiem-hichri <wiemhichri794@gmail.com>
2025-05-01 04:49:55 +0200

    recently

--- backend/routes/smsRoutes.js
+++ backend/routes/smsRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const { isAuthentificated } = require('../middlewares/authMiddleware');
 const { smsCount,
     insertSMS,
     getSMS,
     deleteSMS,
     smsSent} = require('../controllers/smsController');
 
-router.get('/count', smsCount);
-router.post('/insert', insertSMS);
-router.get('/recent', getSMS);
-router.delete('/delete', deleteSMS);
-router.post('/sent',smsSent)
+router.get('/count', isAuthentificated, smsCount);
+router.post('/insert', isAuthentificated, insertSMS);
+router.get('/recent',isAuthentificated, getSMS);
+router.delete('/delete',isAuthentificated, deleteSMS);
+router.post('/sent',isAuthentificated, smsSent)
 
 module.exports = router;
