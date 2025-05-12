4380a18b32ee2225e0ea1e5f36b1c40c3ab01ff8
ImperialDawn <ghaithdormech0@gmail.com>
2025-05-12 11:30:53 +0200

    historique

--- frontend/src/app/parametre/historique/historique.component.ts
+++ frontend/src/app/parametre/historique/historique.component.ts
@@ -2,16 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import * as XLSX from 'xlsx';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-historique',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './historique.component.html',
   styleUrl: './historique.component.scss'
 })
 export class HistoriqueComponent implements OnInit {
   historique: any[] = [];
+  filteredHistorique: any[] = [];
+  searchText: string = '';
 
   constructor(private http: HttpClient) {}
 
@@ -19,14 +22,25 @@ export class HistoriqueComponent implements OnInit {
     this.http.get<{ messages: any[] }>('http://localhost:3000/api/historique', {
       withCredentials: true
     }).subscribe({
-      next: (res) => this.historique = res.messages,
+      next: (res) => {
+        this.historique = res.messages;
+        this.filteredHistorique = res.messages;
+      },
       error: (err) => console.error('Erreur chargement historique:', err)
     });
   }
 
+  filterHistorique() {
+    const text = this.searchText.toLowerCase();
+    this.filteredHistorique = this.historique.filter(item =>
+      item.user_name?.toLowerCase().includes(text) ||
+      item.DestinationNumber?.toLowerCase().includes(text) ||
+      item.SendingDateTime?.toLowerCase().includes(text)
+    );
+  }
+
   exportToExcel(): void {
-    /* Create a worksheet */
-    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.historique.map(item => ({
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.filteredHistorique.map(item => ({
       'Utilisateur': item.user_name,
       'Date d\'envoi': item.SendingDateTime,
       'Destinataire': item.DestinationNumber,
@@ -34,11 +48,8 @@ export class HistoriqueComponent implements OnInit {
       'Statut': item.Status
     })));
 
-    /* Create workbook */
     const workbook: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, 'Historique');
-
-    /* Generate Excel file and download */
     const date = new Date().toISOString().slice(0, 10);
     XLSX.writeFile(workbook, `historique_sms_${date}.xlsx`);
   }
